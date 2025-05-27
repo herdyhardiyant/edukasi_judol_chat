34b57cc6305fa516d6d8ecc14285adb8df2f19fe
Herdy <54350341+herdyhardiyant@users.noreply.github.com>
2025-05-27 16:39:21 +0700

    refactor router/web.py

--- app/routes/web.py
+++ app/routes/web.py
@@ -11,38 +11,47 @@ from datetime import datetime, timezone
 
 templates = Jinja2Templates(directory="app/templates")
 router = APIRouter()
+user_session_key_name = "user_session_id"
 
 
+def extend_chat_session(db_session: SessionDependency, chat_session: ChatSession):
+    chat_session.last_active_at = datetime.now(timezone.utc)
+    db_session.add(chat_session)
+    db_session.commit()
+    db_session.refresh(chat_session)
+    
+    
+def get_chat_session(request: Request, cookie_name: str, db_session: SessionDependency) -> ChatSession | None:
+    cookie_session_id = request.cookies.get(cookie_name)
+    chat_session: Optional[ChatSession] = None
+    
+    statement = select(ChatSession).where(ChatSession.user_session == cookie_session_id)
+    chat_session = db_session.exec(statement).first()
+    
+    if chat_session:
+        extend_chat_session(db_session, chat_session)
+        print(f"Found existing chat session: {chat_session.user_session}")
+    
+    return chat_session
+        
+def create_new_chat_session_to_db(db_session: SessionDependency):
+    new_session_uuid = str(uuid4())
+    chat_session = ChatSession(user_session=new_session_uuid)
+    db_session.add(chat_session)
+    db_session.commit()
+    db_session.refresh(chat_session)
+    print(f"Set cookie name={user_session_key_name} value={new_session_uuid}")
+    return new_session_uuid
 
 @router.get("/")
 async def home(request: Request, response: Response, db_session: SessionDependency ):
-    user_session_key_name = "user_session_id"
-    cookie_session_id = request.cookies.get(user_session_key_name)
-    chat_session: Optional[ChatSession] = None
-    
     template_response = templates.TemplateResponse("index.html", {"request": request, "message": "Welcome"})
     
-    if cookie_session_id:
-        statement = select(ChatSession).where(ChatSession.user_session == cookie_session_id)
-        chat_session = db_session.exec(statement).first()
+    chat_session = get_chat_session(request, user_session_key_name, db_session)
     
     if not chat_session:
-        print(f"user don't have session in their cookies")
-        new_session_uuid = str(uuid4())
-        chat_session = ChatSession(user_session=new_session_uuid)
-        db_session.add(chat_session)
-        db_session.commit()
-        db_session.refresh(chat_session)
-        print(response.headers)
-        print(f"Set cookie name={user_session_key_name} value={new_session_uuid}")
+        new_session_uuid = create_new_chat_session_to_db(db_session)
         template_response.set_cookie(key=user_session_key_name, value=new_session_uuid, httponly=True, secure=False, samesite="lax")
-        
-    else:
-        chat_session.last_active_at = datetime.now(timezone.utc)
-        db_session.add(chat_session)
-        db_session.commit()
-        db_session.refresh(chat_session)
-        print(f"Found existing chat session: {chat_session.user_session}")
 
     return template_response
 
