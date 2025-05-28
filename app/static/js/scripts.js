07c6b29b929d3ba6d1ce52252dc673968788649d
Herdy <54350341+herdyhardiyant@users.noreply.github.com>
2025-05-28 17:20:19 +0700

    chat main structure

---
+++ app/static/js/scripts.js
@@ -0,0 +1,7 @@
+
+const inputBoxDiv = document.getElementById("input-box")
+
+const chatOffset = inputBoxDiv.offsetHeight + 40
+
+const emptyOffsetDiv = document.getElementById("empty-offset")
+emptyOffsetDiv.style.height = `${chatOffset}px`
