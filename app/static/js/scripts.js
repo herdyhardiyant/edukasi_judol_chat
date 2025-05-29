7bb7e93a174d83abc7be0a223b3910be980ebd23
Herdy <54350341+herdyhardiyant@users.noreply.github.com>
2025-05-29 11:21:35 +0700

    auto scroll & favicon

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -3,5 +3,6 @@ const inputBoxDiv = document.getElementById("input-box")
 
 const chatOffset = inputBoxDiv.offsetHeight + 40
 
-const emptyOffsetDiv = document.getElementById("empty-offset")
+const emptyOffsetDiv = document.getElementById("chat-empty-offset")
 emptyOffsetDiv.style.height = `${chatOffset}px`
+emptyOffsetDiv.scrollIntoView({behavior: 'smooth', block: 'start'})
